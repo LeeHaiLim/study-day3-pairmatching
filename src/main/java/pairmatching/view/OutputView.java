5b8c218f07bf105492351c4d74ebec1ead69061c
LeeHaiLim <hailim1997@gmail.com>
2022-12-11 16:38:47 +0900

    feat(view): add print methods
    Added methods:
    - print pair init result
    - print error message

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -22,4 +22,12 @@ public class OutputView {
             System.out.println(String.join(" : ", pair.getCrewNames()));
         }
     }
+
+    public void printPairInitResult () {
+        System.out.println("초기화 되었습니다.");
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println("[ERROR] " + message);
+    }
 }
