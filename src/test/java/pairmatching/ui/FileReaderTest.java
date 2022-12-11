38f6b6b7d39d203d954622268c17f21ba98d66da
Limjongho <whdgh9873@icloud.com>
2022-12-11 13:30:33 +0900

    test: 백엔드 크루 파일 읽어오기 테스트

---
+++ src/test/java/pairmatching/ui/FileReaderTest.java
@@ -0,0 +1,20 @@
+package pairmatching.ui;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileReaderTest {
+
+    @DisplayName("백엔드 크루 파일 읽어오기 테스트")
+    @Test
+    void readBackFileTest() {
+        List<String> strings = FileReader.readBackCrew();
+
+        Assertions.assertThat(strings.size()).isEqualTo(20);
+    }
+}
