1223608b36717b7f9a0467c7ce9f7df926fbc770
EUNJEONGMUN <munej26@gmail.com>
2022-12-11 15:00:41 +0900

    feat: 코스별 크루들 이름을 가져오는 기능

--- src/main/java/pairmatching/domain/Crew.java
+++ src/main/java/pairmatching/domain/Crew.java
@@ -1,5 +1,7 @@
 package pairmatching.domain;
 
+import java.util.Objects;
+
 public class Crew {
     private Course course;
     private String name;
@@ -11,5 +13,39 @@ public class Crew {
 
     public static Crew of(Course course, String name) {
         return new Crew(course, name);
+    }
+
+    public boolean isBackendCrew() {
+        return course == Course.BACKEND;
+    }
+
+    public boolean isFrontendCrew() {
+        return course == Course.FRONTEND;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course, name);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Crew)) {
+            return false;
+        }
+        Crew crew = (Crew) obj;
+        if (this.course.equals(crew.course) && this.name.equals(crew.name)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return course.name()+" " +name;
     }
 }
--- src/main/java/pairmatching/repository/CrewRepository.java
+++ src/main/java/pairmatching/repository/CrewRepository.java
@@ -4,11 +4,26 @@ import pairmatching.domain.Crew;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CrewRepository {
     private static final List<Crew> crews = new ArrayList<>();
 
     public void addCrew(Crew crew) {
         crews.add(crew);
+    }
+
+    public List<String> getBackendCrews() {
+        return crews.stream()
+                .filter(crew -> crew.isBackendCrew())
+                .map(Crew::getName)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getFrontendCrews() {
+        return crews.stream()
+                .filter(crew -> crew.isFrontendCrew())
+                .map(Crew::getName)
+                .collect(Collectors.toList());
     }
 }
