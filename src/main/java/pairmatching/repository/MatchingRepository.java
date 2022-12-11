8d25a8cbdf396bf4b980a4c73033153f2e10a0da
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-12-11 15:10:16 +0900

    feat(MatchingRepository) : 매칭 정보가 있는지 확인하는 기능 구현

--- src/main/java/pairmatching/repository/MatchingRepository.java
+++ src/main/java/pairmatching/repository/MatchingRepository.java
@@ -19,5 +19,12 @@ public class MatchingRepository {
         return null;
     }
 
-
+    public boolean isAlreadyExist(Course course, Mission mission) {
+        for (Matching matching : matchingList) {
+            if (matching.isSameMatching(course, mission)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
