a1f45f7cee190ec43a0795b692c3774b2473ba11
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-12-11 16:42:42 +0900

    feat(InputView) : 각 메서드마다 알맞은 입력 요청 메세지를 출력하도록 함

--- src/main/java/pairmatching/view/input/InputView.java
+++ src/main/java/pairmatching/view/input/InputView.java
@@ -1,18 +1,38 @@
 package pairmatching.view.input;
 
+import camp.nextstep.edu.missionutils.Console;
 import pairmatching.constant.Function;
 
 public class InputView {
 
+    private static final String REQUEST_FUNCTION = "기능을 선택하세요.";
+    private static final String REQUEST_MISSION = "과정, 레벨, 미션을 선택하세요.";
+    private static final String MISSION_FORM_EXAMPLE = "ex) 백엔드, 레벨1, 자동차경주";
+    private static final String REQUEST_RETRY_MATCHING = "\n매칭 정보가 있습니다. 다시 매칭하시겠습니까?";
+    private static final String RETRY_MATCHING_FORM_EXAMPLE = "네 | 아니요";
+
     public Function readFunction() {
+        print(REQUEST_FUNCTION);
         return null;
     }
 
     public String readMission() {
+        print(REQUEST_MISSION);
+        print(MISSION_FORM_EXAMPLE);
         return null;
     }
 
     public String readRetryMatching() {
+        print(REQUEST_RETRY_MATCHING);
+        print(RETRY_MATCHING_FORM_EXAMPLE);
         return null;
+    }
+
+    private String read() {
+        return Console.readLine();
+    }
+
+    private void print(String message) {
+        System.out.println(message);
     }
 }
