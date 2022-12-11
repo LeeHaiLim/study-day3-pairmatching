dcf825ae21306d1d184bd4e82f2d13d306cbf977
EUNJEONGMUN <munej26@gmail.com>
2022-12-11 13:22:26 +0900

    test: 크루 파일 입출력 테스트

--- src/test/java/pairmatching/service/CrewSaveServiceTest.java
+++ src/test/java/pairmatching/service/CrewSaveServiceTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package pairmatching.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 class CrewSaveServiceTest {
-  
+    private CrewSaveService crewSaveService = new CrewSaveService();
+
+    @DisplayName("백엔드 크루 출력 테스트")
+    @Test
+    void getBackendCrew() {
+        List<String> backendCrew = crewSaveService.getBackendCrew();
+        Assertions.assertThat(backendCrew.size()).isEqualTo(20);
+    }
+
+    @DisplayName("프론트엔드 크루 출력 테스트")
+    @Test
+    void getFrontendCrew() {
+        List<String> frontendCrew = crewSaveService.getFrontendCrew();
+        Assertions.assertThat(frontendCrew.size()).isEqualTo(15);
+    }
 }
