b54c927ac52421600506f4f9414ce0df74fe761b
Limjongho <whdgh9873@icloud.com>
2022-12-11 14:56:23 +0900

    test: 미션 매칭 기능 테스트 추가

--- src/test/java/pairmatching/service/MissionServiceTest.java
+++ src/test/java/pairmatching/service/MissionServiceTest.java
@@ -4,8 +4,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pairmatching.domain.Mission;
+import pairmatching.domain.Part;
 import pairmatching.repository.missionrepository.MemoryMissionRepository;
 import pairmatching.repository.missionrepository.MissionRepository;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,10 +22,16 @@ class MissionServiceTest {
     void setUp() {
         missionRepository = new MemoryMissionRepository();
         missionService = new MissionService(missionRepository);
+        missionService.init();
     }
 
     @Test
-    void createMission() {
+    void createMissionPairMatching() {
+        missionService.createMissionPairMatching(Part.FRONT_END, "자동차경주");
+
+        Mission racing = missionRepository.findByName("자동차경주", Part.FRONT_END).get();
+
+        Assertions.assertThat(missionRepository.isMatched(racing)).isEqualTo(true);
     }
 
     @Test
