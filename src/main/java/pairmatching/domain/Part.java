5b5026f1b678a92520922c727a5db98ba012c180
Limjongho <whdgh9873@icloud.com>
2022-12-11 13:43:03 +0900

    feat(Part): 파트 상수 구현 완료

--- src/main/java/pairmatching/domain/Part.java
+++ src/main/java/pairmatching/domain/Part.java
@@ -1,6 +1,21 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum Part {
-    FRONT_END,
-    BACK_END
+    FRONT_END("프론트엔드"),
+    BACK_END("백엔드");
+
+    private final String partName;
+
+    Part(String partName) {
+        this.partName = partName;
+    }
+
+    public static Part part(String partInput) {
+        return Arrays.stream(Part.values())
+                .filter(part -> part.partName.equals(partInput))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR]"));
+    }
 }
