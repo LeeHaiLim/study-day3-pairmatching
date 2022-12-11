f3a3f80caa8d2f74283ad6c7425a8be981981ccf
LeeHaiLim <hailim1997@gmail.com>
2022-12-11 12:46:13 +0900

    feat(view): implement class input view
    Added method:
    - read feature

---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,15 @@
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String readFeature() {
+        System.out.println("기능을 선택하세요.\n"
+                + "1. 페어 매칭\n"
+                + "2. 페어 조회\n"
+                + "3. 페어 초기화\n"
+                + "Q. 종료");
+
+        return Console.readLine();
+    }
+}
