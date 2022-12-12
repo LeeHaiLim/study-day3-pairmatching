b3d69a9dda8317b1a2d54cfac20f24e31a2e4699
EUNJEONGMUN <munej26@gmail.com>
2022-12-13 00:35:28 +0900

    refactor: 랜덤 페어 생성 기능 RandomPairMaker으로 이동

--- src/main/java/pairmatching/service/MatchingService.java
+++ src/main/java/pairmatching/service/MatchingService.java
@@ -1,16 +1,12 @@
 package pairmatching.service;
 
-import pairmatching.domain.Crew;
 import pairmatching.domain.MissionGroup;
 import pairmatching.domain.Pair;
 import pairmatching.repository.CrewRepository;
 import pairmatching.repository.MatchingRepository;
 import pairmatching.util.RandomPairMaker;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class MatchingService {
     private CrewRepository crewRepository = new CrewRepository();
@@ -21,15 +17,15 @@ public class MatchingService {
         if (crews.size() < 2) {
             throw new IllegalArgumentException("2명 이상일 때 매칭할 수 있습니다.");
         }
-            return getRandomOrder(missionGroup, crews);
         if (matchingRepository.getSameLevelPairs(missionGroup)==null) {
+            return RandomPairMaker.getRandomPair(missionGroup, crews);
         }
         return getValidMatchingPairs(missionGroup, crews);
     }
 
     private List<Pair> getValidMatchingPairs(MissionGroup missionGroup, List<String> crews) {
         for (int i = 0; i < 3; i++) {
-            List<Pair> randomOrder = getRandomOrder(missionGroup, crews);
+            List<Pair> randomOrder = RandomPairMaker.getRandomPair(missionGroup, crews);
             List<Pair> repositoryPairs = matchingRepository.getSameLevelPairs(missionGroup);
             if (!hasSamePair(randomOrder, repositoryPairs)) {
                 return randomOrder;
@@ -38,29 +34,10 @@ public class MatchingService {
         throw new IllegalArgumentException("매칭할 수 없습니다.");
     }
 
-    private List<Pair> getRandomOrder(MissionGroup missionGroup, List<String> crews) {
-        return convertToPair(RandomPairMaker.getRandomOrder(crews).stream()
-                .map(name -> Crew.of(missionGroup.getCourse(), name))
-                .collect(Collectors.toList()));
-    }
-
     private boolean hasSamePair(List<Pair> randomOrder, List<Pair> pairs) {
         return randomOrder.stream()
                 .anyMatch(randomPair -> pairs.stream()
                         .anyMatch(pair -> pair.isSamePair(randomPair)));
-    }
-
-    private List<Pair> convertToPair(List<Crew> crews) {
-        List<Pair> pairs = new ArrayList<>();
-        int size = crews.size() / 2;
-        for (int i = 0; i < size * 2; i += 2) {
-            pairs.add(Pair.of(crews.subList(i, i + 2)));
-        }
-        if (crews.size() % 2 != 0) {
-            Pair lastPair = pairs.get(pairs.size() - 1);
-            lastPair.addPair(crews.get(crews.size() - 1));
-        }
-        return pairs;
     }
 
     public List<Pair> getMatchResult(MissionGroup missionGroup) {
--- src/main/java/pairmatching/util/RandomPairMaker.java
+++ src/main/java/pairmatching/util/RandomPairMaker.java
@@ -1,12 +1,36 @@
 package pairmatching.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import pairmatching.domain.Crew;
+import pairmatching.domain.MissionGroup;
+import pairmatching.domain.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RandomPairMaker {
 
-    public static List<String> getRandomOrder(List<String> crews) {
+    public static List<Pair> getRandomPair(MissionGroup missionGroup, List<String> crews) {
+        return convertToPair(shuffleOrder(crews).stream()
+                .map(name -> Crew.of(missionGroup.getCourse(), name))
+                .collect(Collectors.toList()));
+    }
+
+    private static List<String> shuffleOrder(List<String> crews) {
         return Randoms.shuffle(crews);
+    }
+
+    private static List<Pair> convertToPair(List<Crew> crews) {
+        List<Pair> pairs = new ArrayList<>();
+        int size = crews.size() / 2;
+        for (int i = 0; i < size * 2; i += 2) {
+            pairs.add(Pair.of(crews.subList(i, i + 2)));
+        }
+        if (crews.size() % 2 != 0) {
+            Pair lastPair = pairs.get(pairs.size() - 1);
+            lastPair.addPair(crews.get(crews.size() - 1));
+        }
+        return pairs;
     }
 }
